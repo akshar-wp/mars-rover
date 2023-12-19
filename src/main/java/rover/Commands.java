471e0a0117854eb035eb4e0bdec6ee243feac751
Akshar <aksharbhagwandin@gmail.com>
2023-12-20 00:06:52 +0200

    add instruction commands

---
+++ src/main/java/rover/Commands.java
@@ -0,0 +1,20 @@
+package rover;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class Commands {
+
+    public static List<Command> commands(String commands) {
+        List<Command> result = new ArrayList<>();
+        for (Character c : commands.toCharArray()) {
+            switch (c) {
+                case 'L' -> result.add(Command.LEFT);
+                case 'R' -> result.add(Command.RIGHT);
+                case 'M' -> result.add(Command.MOVE);
+                default -> throw new UnknownCommandException(c);
+            }
+        }
+        return result;
+    }
+}
