3ef2f3211a47623f3f04daf8f2a0b2bbfd640171
Akshar <aksharbhagwandin@gmail.com>
2023-12-20 01:01:30 +0200

    add logic to main

--- src/main/java/rover/Main.java
+++ src/main/java/rover/Main.java
@@ -1,6 +1,29 @@
 package rover;
 
+import java.util.List;
+
 public class Main {
     public static void main(String[] args) {
+        Plateau plateau = InputUtil.parsePlateauInput("5 5");
+
+        Rover rover1 = InputUtil.createRover()  ;
+        InputUtil.parseRoverToPlateau(plateau, rover1, "1 2 N");
+        List<Command> commands1 = InputUtil.parseCommands("LMLMLMLMM");
+
+        Rover rover2 = InputUtil.createRover();
+        InputUtil.parseRoverToPlateau(plateau, rover2, "3 3 E");
+        List<Command> commands2 = InputUtil.parseCommands("MMRMMRMRRM");
+
+//        Rover rover3 = InputUtil.createRover();
+//        InputUtil.parseRoverToPlateau(plateau, rover3, "6 3 E");
+//        List<Command> commands3 = InputUtil.parseCommands("MMRMMRMRRM");
+
+        rover1.executeCommands(commands1);
+        rover2.executeCommands(commands2);
+//        rover3.executeCommands(commands3);
+
+        System.out.println(rover1);
+        System.out.println(rover2);
+//        System.out.println(rover3);
     }
 }
